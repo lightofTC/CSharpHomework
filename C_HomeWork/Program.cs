de91c3285929e970e0519372701c23baeae526a1
lightofTC <1727679761@qq.com>
2018-09-11 22:17:26 +0800

    添加项目文件。

---
+++ C_HomeWork/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace C_HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = "";
+            int m = 0, n = 0;
+            Console.WriteLine("Please input an int as you like:");
+            number = Console.ReadLine();
+            m = Int32.Parse(number);
+            Console.WriteLine("Please input an int again as you like:");
+            number = Console.ReadLine();
+            n = Int32.Parse(number);
+            m = m * n;
+            Console.WriteLine("The anwser is: " + m );
+        }
+    }
+}
---
+++ Homework1/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Homework1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.number1 = new System.Windows.Forms.TextBox();
+            this.number2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // number1
+            // 
+            this.number1.Location = new System.Drawing.Point(276, 42);
+            this.number1.Name = "number1";
+            this.number1.Size = new System.Drawing.Size(185, 25);
+            this.number1.TabIndex = 0;
+            this.number1.TextChanged += new System.EventHandler(this.number1_TextChanged);
+            // 
+            // number2
+            // 
+            this.number2.Location = new System.Drawing.Point(276, 92);
+            this.number2.Name = "number2";
+            this.number2.Size = new System.Drawing.Size(185, 25);
+            this.number2.TabIndex = 1;
+            this.number2.TextChanged += new System.EventHandler(this.number2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(311, 147);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 50);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "求积";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(341, 256);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(321, 228);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 25);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(961, 303);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.number2);
+            this.Controls.Add(this.number1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox number1;
+        private System.Windows.Forms.TextBox number2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
