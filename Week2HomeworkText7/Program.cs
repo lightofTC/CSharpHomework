50f1a0608207d6b6044b9bc599bad9ae2c18b3d5
lightofTC <1727679761@qq.com>
2018-09-18 21:56:39 +0800

    h

---
+++ Week2HomeworkText7/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2HomeworkText7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 8, 2, 5, 9, 3, 7 };
+            int i, k = 0;
+            int length = array.Length;
+              
+
+            for (i = 0; i < length - 1; i++)
+            {
+                if (array[i] > array[i + 1])
+                {
+                    k = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = k;
+                }
+            }
+            Console.WriteLine("最大值：" + array[length - 1]);
+
+
+            for (i = 0; i < length - 1; i++)
+            {
+                if (array[i] < array[i + 1])
+                {
+                    k = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = k;
+                }
+            }
+            Console.WriteLine("最小值：" + array[length - 1]);
+
+
+            for(i = 0; i < length - 1; i++)
+            {
+                k = k + array[i];
+            }
+            Console.WriteLine("所有元素的和：" + k);
+            double j = k / length;
+            Console.WriteLine("平均值：" + j);
+        }
+    }
+}
---
+++ Week2HomeworkText9/Text9.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2HomeworkText9
+{
+    class Text9
+    {
+        static void Main(string[] args)
+        {
+            int i ,j= 0;
+            int[] array=new int [101];
+
+
+            for (i = 1; i <=100; i++)
+            {
+                array[i] = i;
+            }
+            array[1] = 0;
+
+
+            for (i = 2; i <= 100; i++)
+            {
+                if(array[i] != 0)
+                {
+                    for (j = i + 1; j <= 100; j++)
+                    {
+                        if(array[j] != 0 && array[j] % array[i] == 0)
+                        {
+                            array[j] = 0;
+                        }
+                    }
+                }
+            }
+
+
+            Console.Write("素数有：");
+            for (i = 2; i < 101; i++)
+            {
+                if(array[i] != 0)
+                {
+                    Console.Write(array[i] + " ");
+                }
+            }
+        }
+    }
+}
