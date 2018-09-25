94a0472a0b595c7e45972afac5cf56ebf3139da5
lightofTC <1727679761@qq.com>
2018-09-26 00:38:12 +0800

    9月25日第三次作业

---
+++ Week3Homework/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape shape = null;
+            Console.WriteLine("请从以下选项中输入您所需要计算的图形的面积：“Circle”(圆)，“Triangle”（三角形），“Rectangle”（长方形)，“Square”(正方形）");
+            string s = Console.ReadLine();
+            shape = ShapeFactory.GetShape(s);
+            shape.Print(s);
+        }
+    }
+}
