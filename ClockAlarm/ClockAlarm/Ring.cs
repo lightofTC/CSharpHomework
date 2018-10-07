803c71b49384ed4e67ffc3f85c2ebd501cf7d790
lightofTC <1727679761@qq.com>
2018-10-07 23:06:02 +0800

    Homework4

---
+++ ClockAlarm/ClockAlarm/Ring.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockAlarm
+{
+    public delegate void RingHandler();
+    public class Ring
+    {
+        public event RingHandler SendMessage;
+        public void OnSendMessage()
+        {
+            SendMessage();
+        }
+    }
+
+    public class Me
+    {
+        public void HandleEvent()
+        {
+            Console.WriteLine("时间到！");
+        }
+        public void Register(Ring ring)
+        {
+            ring.SendMessage += new RingHandler(HandleEvent);
+        }
+    }
+}
